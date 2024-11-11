6343465e380a555eb621a853c90b080c760d8f99
REXRITZ <rmbhadane33@gmail.com>
2024-11-11 12:40:03 +0530

    checkout, commit done

--- client.cpp
+++ client.cpp
@@ -426,8 +426,8 @@ int main(int argc, char **argv) {
 
     // Client side commands:
     // Done -- listall - to list all files present on server with current readers/writers count.
-    // checkout - download file from server and store on client side.
-    // commit - update file on server -> syncronized
+    // Done -- checkout - download file from server and store on client side.
+    // Done -- commit - update file on server -> syncronized
     // Done -- add - add a new file to the server
     // delete - delete a file on the server
     

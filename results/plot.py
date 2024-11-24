c188c5e5247d7f0d49ae0635ca74b04cbff431d5
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2024-11-24 18:11:38 +0530

    Update plot.py.

--- results/plot.py
+++ results/plot.py
@@ -22,12 +22,23 @@ for line in file.readlines():
     throughput.append(float(info[2]))
 
 
-plt.plot(labels, responsetime)
+# plt.plot(labels, responsetime)
+plt.plot(responsetime, throughput)
 
-plt.xlabel("#clients")
-plt.ylabel("Time taken (sec)")
-plt.title("#clients vs Time taken")
+
+# plt.xlabel("#clients")
+# plt.ylabel("Time taken (sec)")
+# plt.title("#clients vs Time taken")
+
+# plt.xlabel("#clients")
+# plt.ylabel("Requests/time")
+# plt.title("#clients vs requests/time")
+
+plt.xlabel("#requests")
+plt.ylabel("throughput (requests/time)")
+plt.title("#requests vs requests/time")
+
 # plt.xlim(left=0)
 # plt.ylim(bottom=0)
 plt.grid()
-plt.savefig('plot.png')
+plt.savefig('results/plot.png')

bc43fcb2da1d55c6dce3f4e85d51e1bf9251b3d0
REXRITZ <rmbhadane33@gmail.com>
2024-11-24 12:49:47 +0530

    updated load_test.sh

--- load_test.sh
+++ load_test.sh
@@ -1,9 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./load_test.sh <server_port>"
+    exit 1
+fi
 
 # # Configuration
 SERVER_IP="127.0.0.1"  # Replace with actual server IP
 PORT=$1  # Replace with actual port number
-MAX_CLIENTS=2
+MAX_CLIENTS=10
 MAX_COMMANDS=10
 
 
@@ -27,45 +32,44 @@ fi
 #         echo "$username" >> "$file"
 #         echo "$password" >> "$file"
 #         for ((j=1; j<=MAX_COMMANDS; j++)); do
-#             echo "checkout abc.txt" >> "$file"
+#             echo "listall" >> "$file"
 #         done
 #         echo "quit" >> "$file"
 
 # done
-
+# exit 1
 
 for ((clients=1; clients<=MAX_CLIENTS; clients++)); do
-    echo "Running $clients client(s)"
-
+    start_time=$(date +%s)
     # Run clients in parallel and capture durations
     for ((i=1; i<=$clients; i++)); do
         
         (
         
-        start_time=$(date +%s)
+        
         # Simulate client interactions
-        ./client "$SERVER_IP" "$PORT" < "input/inp_1.txt"
+        ./client "$SERVER_IP" "$PORT" < "input/inp_$i.txt"
         # sleep 1
 
-        end_time=$(date +%s.%N)
-        
-        total_time=$(echo "$end_time - $start_time - 1" | bc)
-        
-        echo "$total_time" >> "$temp_file"
         ) &
     done
 
     wait
 
-
-    total_time=0
-    while read -r line
-    do
-        total_time=$(echo "$total_time + $line" | bc -l)
-    done < "$temp_file"
-    echo "total: $total_time"
-    
+    end_time=$(date +%s.%N)    
+    total_time=$(echo "$end_time - $start_time" | bc)
+    # echo "$total_time" >> "$temp_file"
     echo "$clients $total_time" >> "$results_file"
+
+
+    # total_time=0
+    # while read -r line
+    # do
+    #     total_time=$(echo "$total_time + $line" | bc -l)
+    # done < "$temp_file"
+    # echo "total: $total_time"
+    
+    # echo "$clients $total_time" >> "$results_file"
     
 
 done
